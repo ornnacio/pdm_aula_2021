f6bf22592ea559fc3ef540139256c0a46df73b4b
Jackson <lordjackson@gmail.com>
2021-04-01 12:05:36 -0300

    Hello Word React-Native

--- App.js
+++ App.js
@@ -1,66 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Text, View, Image, TextInput, Button, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 import styles from "./estilo";
 
-const Carro = (props) => {
-  return (
-    <View style={[props.direcao]}>
-      <Text style={props.styleCarro}>Seu carro é {props.nome}</Text>
-    </View>
-  );
-};
-
-const mensagem = (texto) => {
-  return alert("Sua mensagem: " + texto);
-};
 export default function App() {
-  const [descricao, setDescricao] = useState("");
   return (
     <View style={styles.container}>
-      <Text style={styles.azul}>Hello world!</Text>
-      <Image
-        source={{ uri: "https://reactnative.dev/docs/assets/p_cat1.png" }}
-        style={{ width: 200, height: 200 }}
-      />
-      <Carro
-        nome="Camaro"
-        styleCarro={{ backgroundColor: "gray", color: "yellow" }}
-        direcao={{ alignSelf: "flex-start" }}
-      />
-      <Carro
-        nome="Corsa"
-        styleCarro={{
-          backgroundColor: "orange",
-          color: "green",
-        }}
-        direcao={{ alignSelf: "flex-end" }}
-      />
-      <TextInput
-        style={{ borderWidth: 1, height: 40, width: 200, borderColor: "gray" }}
-        placeholder="Nome"
-        onChangeText={(text) => setDescricao(text)}
-      />
-      <Text style={styles.vermelho}>{descricao}</Text>
-      <Text style={stylesScreen.textoDescricao}>
-        Descrição teste teste teste teste teste teste
-      </Text>
-      <Button
-        style={{ backgroundColor: "green" }}
-        title="Clique Aqui"
-        color="green"
-        onPress={() => mensagem(descricao)}
-      />
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const stylesScreen = StyleSheet.create({
-  textoDescricao: {
-    backgroundColor: "#c8c8c8",
-    color: "orange",
-    fontSize: 22,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
