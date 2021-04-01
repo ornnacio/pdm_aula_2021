e054724c2b6a206511cad192cc256123e6d01eaf
Jackson <lordjackson@gmail.com>
2021-04-01 12:17:55 -0300

    Introdução ao React-Native - Styles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pdm_aula_2021
+
 Repositorio Aulas de programação Mobile com React-Native
+
+Tag 0.1
+<img src="https://user-images.githubusercontent.com/6691621/113315040-3f2bd200-92e3-11eb-9b01-c996897a7e2b.png?v=4&s=100" width="200" height="400" />
---
+++ estilo.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  azul: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  vermelho: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+});
+
+export default styles;
