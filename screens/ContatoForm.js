1affad915463dc9fa1008ff348d3c2c1ec781bf3
Jackson <lordjackson@gmail.com>
2021-04-28 10:47:56 -0300

    Listar dados de um formulário

--- screens/ContatoForm.js
+++ screens/ContatoForm.js
@@ -1,37 +1,87 @@
 import * as React from "react";
-import { TextInput, Text, Button } from "react-native-paper";
+import {
+  Card,
+  Divider,
+  TextInput,
+  Text,
+  Title,
+  Paragraph,
+  Button,
+  List,
+} from "react-native-paper";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class ContatoForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { num01: null, num02: null, result: null };
+    this.state = {
+      nome: null,
+      telefone: null,
+      dataNascimento: null,
+      contatoList: [],
+    };
   }
-  soma = () => {
+  componentDidMount() {
+    this.getData();
+  }
+  getData = () => {
+    let contato = {
+      nome: this.state.nome,
+      telefone: this.state.telefone,
+      dataNascimento: this.state.dataNascimento,
+    };
+
+    if (this.state.nome) {
+      this.setState({ contatoList: [...this.state.contatoList, contato] });
+    }
+
     this.setState({
-      result: parseInt(this.state.num01) + parseInt(this.state.num02),
+      nome: null,
+      telefone: null,
+      dataNascimento: null,
     });
+    console.log(this.state.contatoList);
   };
 
   render() {
     return (
       <>
         <TextInput
-          label="Numero 01"
-          value={this.state.num01}
-          onChangeText={(text) => this.setState({ num01: text })}
+          label="Nome"
+          value={this.state.nome}
+          onChangeText={(text) => this.setState({ nome: text })}
         />
         <TextInput
-          label="Numero 02"
-          value={this.state.num02}
-          onChangeText={(text) => this.setState({ num02: text })}
+          label="Telefone"
+          value={this.state.telefone}
+          onChangeText={(text) => this.setState({ telefone: text })}
         />
-        <Text style={{ textAlign: "center", fontSize: 24 }}>
-          {this.state.result}
-        </Text>
-        <Button mode="contained" onPress={() => this.soma()}>
-          <Icon name="plus"></Icon> Calcular
+        <TextInput
+          label="Data Nascimento"
+          value={this.state.dataNascimento}
+          onChangeText={(text) => this.setState({ dataNascimento: text })}
+        />
+
+        <Button mode="contained" onPress={() => this.getData()}>
+          <Icon name="save"></Icon> Salvar
         </Button>
+        <Card>
+          <Card.Content>
+            <List.Section>
+              <List.Subheader>Listagem Contatos</List.Subheader>
+
+              {this.state.contatoList.map((item, i) => (
+                <>
+                  <Divider />
+                  <Title>{item.nome}</Title>
+                  <Paragraph>{item.telefone}</Paragraph>
+                  <Paragraph>{item.dataNascimento}</Paragraph>
+                  <Divider />
+                </>
+              ))}
+            </List.Section>
+          </Card.Content>
+        </Card>
       </>
     );
   }
@@ -39,26 +89,26 @@ export default class ContatoForm extends React.Component {
 /* 
 //exemplu usando função
 function ContatoForm() {
-  const [num01, setNum01] = useState(0);
-  const [num02, setNum02] = useState(0);
+  const [nome, setNum01] = useState(0);
+  const [telefone, setNum02] = useState(0);
   const [total, setTotal] = useState(0);
 
   const somar = () => {
-    setTotal(num01 + num02);
+    setTotal(nome + telefone);
   };
 
   return (
     <View>
       <TextInput
         label="Número 01"
-        name="num01"
-        value={num01}
+        name="nome"
+        value={nome}
           onChange={ e => setNum01(e.target.value)}
       />
       <TextInput
         label="Número 02"
-        name="num01"
-        value={num02}
+        name="nome"
+        value={telefone}
         onChange={ e => setNum02(e.target.value)}
       />
       <Text>
