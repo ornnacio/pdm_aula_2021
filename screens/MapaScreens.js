793d18a0d11931c326397cf345a130f22ab2729d
Jackson <lordjackson@gmail.com>
2021-06-30 11:05:05 -0300

    P45 - Definindo marcadores no Mapas do React-Native

--- screens/MapaScreens.js
+++ screens/MapaScreens.js
@@ -1,15 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class MapaScreens extends React.Component {
 
     render() {
         return (
             <View style={styles.container}>
-                <MapView style={styles.map} />
-            </View>
+                <MapView
+                    initialRegion={{
+                        latitude: -27.5942369,
+                        longitude: -48.5453289,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                    showsUserLocation={true}
+                    style={styles.map}>
+
+                    <Marker title={"IFSC"} coordinate={{ latitude: -27.5942369, longitude: -48.5453289 }}>
+                        <View style={{ backgroundColor: "red", padding: 2 }}>
+                            <Text>IFSC</Text>
+                        </View>
+                    </Marker>
+                    <Marker title={"UFSC - Centro Universitário"} coordinate={{ latitude: -27.600704, longitude: -48.5227843 }}>
+                        <View style={{ backgroundColor: "green", padding: 2 }}>
+                            <Text>UFSC</Text>
+                        </View>
+                    </Marker>
+
+                    <Marker title={"Praia do Campeche"} coordinate={{ latitude: -27.6611965, longitude: -48.4819824 }}>
+                    </Marker>
+                </MapView>
+            </View >
         );
     }
 }
